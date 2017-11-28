65173d991be2399faf80ff93530c54c4e71d9331
shreysamdani <shrey.samdani@gmail.com>
2017-11-28 15:16:34 -0800

    added tests for remove

--- contracts/MasterStorage.sol
+++ contracts/MasterStorage.sol
@@ -21,11 +21,10 @@ contract MasterStorage {
     _;
   }
 
-
-
-  // NOT PRIVATE FOR TESTING PURPOSES
  	mapping(address => mapping(string => Project)) allProjects;
 
+
+  // commits the new hash of the project
   function commit(string _projectName, string _projectHash) public {
     Project storage proj = allProjects[msg.sender][_projectName];
     proj.current += 1;
@@ -33,25 +32,30 @@ contract MasterStorage {
     proj.hash = _projectHash;
   }
 
+  // pull own project
   function ownerPull(string _projectName) public returns(string) {
     return allProjects[msg.sender][_projectName].hash;
   }
 
+  // pull anyones public project
   function guestPull(string _projectName, address _projectOwner) isPublic(_projectName, _projectOwner) public returns(string) {
     return allProjects[_projectOwner][_projectName].hash;
   }
 
+  // initialize a project
   function init(string _projectName, bool _isPublic) public {
-    if (allProjects[projectOwner][_projectName].exists) {
-      throw;
+    if (allProjects[msg.sender][_projectName].exists) {
+      revert();
     }
     allProjects[msg.sender][_projectName].exists = true;
     allProjects[msg.sender][_projectName].current = 0;
     allProjects[msg.sender][_projectName].isPublic = _isPublic;
   }
 
+  // remove project
   function remove(string _projectName) {
-    delete allProjects[projectOwner][_projectName];
+    allProjects[msg.sender][_projectName].exists = false;
+    delete allProjects[msg.sender][_projectName];
   }
 
   function() payable {
@@ -59,6 +63,8 @@ contract MasterStorage {
   }
 
 // TESTING FUNCTIONS
+// REMOVE WHEN DEPLOYING
+
   function getCurr(string _projectName) public returns(uint) {
     return allProjects[msg.sender][_projectName].current;
   }
@@ -67,8 +73,12 @@ contract MasterStorage {
     return allProjects[msg.sender][_projectName].history[_pastcommit];
   }
 
-  function getHash(string _projectName, uint _pastcommit) public returns(string) {
+  function getHash(string _projectName) public returns(string) {
     return allProjects[msg.sender][_projectName].hash;
+  }
+
+  function exists(string _projectName) public returns(bool) {
+    return allProjects[msg.sender][_projectName].exists;
   }
 
 
--- test/testTemplate.js
+++ test/testTemplate.js
@@ -7,55 +7,65 @@ contract('Decentralized Github Test', function(accounts) {
 
 	var masterstorage
 
-	// beforeEach(async function() {
-	// 	return MasterStorage.new().then(_masterstorage => {
-	// 		masterstorage = _masterstorage;
-	// 		return masterstorage.init("Private Hello", false, {from: accounts[0]}).then(_ => {
-	// 			return masterstorage.init(12345, true, {from: accounts[0]}).then(_ => {
-	// 				pubProject = masterstorage.allProjects[accounts[0]][12345];
-	// 				privProject = masterstorage.allProjects[accounts[0]]["Private Hello"];
-	// 			})
-	// 		})
-	// 	})
-	// });
-
 	beforeEach(async function() {
 		masterstorage = await MasterStorage.new();
 		await masterstorage.init("Private Hello", false, {from: accounts[0]});
 		await masterstorage.init("Public Hello", true, {from: accounts[0]});
-		// pubProject = await masterstorage.allProjects(accounts[0])[12345];
-		// privProject = await masterstorage.allProjects(accounts[0])["Private Hello"];
+		await masterstorage.commit("Public Hello", '1');
+		await masterstorage.commit('Public Hello', '2');
 	});
 
 
 	describe('Test Commit function', function() {
-		it('Number of commits is correct', async function() {
-			let start = await masterstorage.getCurr.call("Public Hello", {from: accounts[0]})
-			assert.equal(start.valueOf(), 0);
-			await masterstorage.commit("Public Hello", '1');
-			await masterstorage.commit('Public Hello', '2');
 
+		it('Number of commits is correct', async function() {
 			let end = await masterstorage.getCurr.call("Public Hello", {from: accounts[0]});
 			assert.equal(end.valueOf(), 2);
 
 		});
 
 		it('Project has the most recent commit at head', async function() {
-			await masterstorage.commit("Public Hello", '1');
-			await masterstorage.commit('Public Hello', '2');
 			let currHash = await masterstorage.getHash.call("Public Hello", {from: accounts[0]});
 			assert.equal(currHash.valueOf(), "2")
 
 		});
 
 		it('Previous commits are still there', async function() {
-			return masterstorage.commit('Public Hello', '1').then(_ => {
-				return masterstorage.commit('Public Hello', '2').then(_ => {
-					assert.equal(pubProject.history[1], '1')
-				})
-			})
+			let past = await masterstorage.getHist.call("Public Hello", 1, {from: accounts[0]})
+			assert.equal(past.valueOf(), "1")
 		});
 
+		it('Project does not show up for a random person', async function() {
+			let exist = await masterstorage.exists.call("Public Hello", {from: accounts[1]})
+			assert.equal(exist.valueOf(), false)
+		});		
+
+	});
+
+	describe('Test Remove function', function() {
+
+		it('Only owner can delete the project', async function() {
+			// make sure project exists
+			let exist = await masterstorage.exists.call("Public Hello")
+			assert.equal(exist.valueOf(), true);
+
+			// make sure a random person cannot delete the project
+			await masterstorage.remove("Public Hello", {from: accounts[1]});
+			let exist2 = await masterstorage.exists.call("Public Hello", {from: accounts[0]});
+			assert.equal(exist2.valueOf(), true);
+
+			// make sure owner can delete the project
+			await masterstorage.remove("Public Hello", {from: accounts[0]});
+			let exist3 = await masterstorage.exists.call("Public Hello", {from: accounts[0]});
+			assert.equal(exist3.valueOf(), false);
+		});
+
+		it('Project is actually deleted', async function() {
+			await masterstorage.remove("Public Hello", {from: accounts[0]});
+			let curr = await masterstorage.getCurr.call("Public Hello", {from: accounts[0]});
+			assert.equal(curr.valueOf(), 0);
+
+		});
 	});
 
 });
